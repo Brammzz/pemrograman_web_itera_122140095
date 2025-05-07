71068219760eba14705e93c327878d82b3731641
Abraham Ganda N. <113507126+Brammzz@users.noreply.github.com>
2025-05-08 01:34:24 +0700

    Add files via upload

---
+++ Abraham_122140095_pertemuan5/sistem_perpustakaan/models/__init__.py
@@ -0,0 +1,7 @@
+from models.library_item import LibraryItem
+from models.book import Book
+from models.magazine import Magazine
+from models.dvd import DVD
+from models.journal import Journal
+
+__all__ = ['LibraryItem', 'Book', 'Magazine', 'DVD', 'Journal']
